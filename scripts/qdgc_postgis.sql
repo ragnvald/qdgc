b0e8ceac795c6ba4dcf4f627c284968b281c5948
Ragnvald Larsen <ragnvald@mindland.com>
2021-01-13 09:38:54 +0100

    Update qdgc_postgis.sql
    Added comments

--- scripts/qdgc_postgis.sql
+++ scripts/qdgc_postgis.sql
@@ -1,11 +1,14 @@
+/* query requirements are PostGIS 3.1 */
 drop table if exists tbl_qdgc;
 create table  tbl_qdgc (qdgc varchar(20), country varchar(50),qdgc_level int, qdgc_cellsize decimal, longitude decimal, latitude decimal, areakm2 decimal, geom geometry);
 
 do $$
 begin
+   /* Establishing counter which defindes the depth of the grid cell creation. Here from 1 (1/2 degree) to 5 (1/32 degree) */
    for counter in 1..5 loop
 	
 	with grid as (
+		/* creating the grid */
   		select (st_squaregrid((1/(2^counter)), st_transform(geom,4326))).* ,name as continent
   		from tbl_countries where name = 'Tanzania'
 	) 
