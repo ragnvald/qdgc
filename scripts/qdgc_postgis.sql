e3ba87fe0f004b0e1150f090498005e270ed5283
Ragnvald Larsen <ragnvald@mindland.com>
2021-01-14 23:16:16 +0100

    Update qdgc_postgis.sql
    First version with correct QDGC string creator.

--- scripts/qdgc_postgis.sql
+++ scripts/qdgc_postgis.sql
@@ -8,16 +8,16 @@ create table  tbl_qdgc (qdgc varchar(20), country varchar(50),qdgc_level int, qd
 do $$
 begin
    /* Establishing counter which defindes the depth of the grid cell creation. Here from 1 (1/2 degree) to 5 (1/32 degree) */
-   for counter in 1..5 loop
+   for counter in 1..4 loop
 	
 	with grid as (
 		/* creating the grid */
   		select (st_squaregrid((1/(2^counter)), st_transform(geom,4326))).* ,name as continent
   		from tbl_countries where name = 'Tanzania'
 	) 
-
+	
 	insert into tbl_qdgc 
-	select '',continent, counter,(1/(2^counter)), ST_X(ST_Centroid(geom)) as lon,ST_Y(ST_Centroid(geom)) as lat,(st_area(st_transform(geom, 102008))/1000000), geom from grid;
+	select qdgc_getlonlat(ST_X(ST_Centroid(geom)),ST_Y(ST_Centroid(geom)))|| qdgc_get_recursivestring(ST_X(ST_Centroid(geom)),ST_Y(ST_Centroid(geom)),counter,''),continent, counter,(1/(2^counter)), ST_X(ST_Centroid(geom)) as lon,ST_Y(ST_Centroid(geom)) as lat,(st_area(st_transform(geom, 102008))/1000000), geom from grid;
 
 	
    end loop;
