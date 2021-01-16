3dc3085e4e11fd24e9094e7c68962d347d5a62d8
Ragnvald Larsen <ragnvald@mindland.com>
2021-01-17 00:14:23 +0100

    Update create_function_qdgc_fillqdgc.sql
    Error in reference til qdgc-level.

--- scripts/create_function_qdgc_fillqdgc.sql
+++ scripts/create_function_qdgc_fillqdgc.sql
@@ -35,7 +35,7 @@ begin
 	) 
 	
 	insert into tbl_qdgc 
-	select qdgc_getqdgc(ST_X(ST_Centroid(geom)),ST_Y(ST_Centroid(geom)),qdgc_level),area_reference, counter,(1/(2^counter)), ST_X(ST_Centroid(geom)) as lon,ST_Y(ST_Centroid(geom)) as lat,(st_area(st_transform(geom, 102008))/1000000), geom from grid;
+	select qdgc_getqdgc(ST_X(ST_Centroid(geom)),ST_Y(ST_Centroid(geom)),counter),area_reference, counter,(1/(2^counter)), ST_X(ST_Centroid(geom)) as lon,ST_Y(ST_Centroid(geom)) as lat,(st_area(st_transform(geom, 102008))/1000000), geom from grid;
 
 	
    end loop;
