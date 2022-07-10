bdf91000cbeb042562a8a9ad7aec6bd60600c517
Ragnvald Larsen <ragnvald@mindland.com>
2022-07-11 00:11:28 +0200

    Err in creating function.
    Name with underscores.

--- scripts/create_function_qdgc_getqdgc.sql
+++ scripts/create_function_qdgc_getqdgc.sql
@@ -1,8 +1,8 @@
--- FUNCTION: public.qdgc_get_qdgc(double precision, double precision, integer)
+-- FUNCTION: public.qdgc_getqdgc(double precision, double precision, integer)
 
--- DROP FUNCTION public.qdgc_get_qdgc(double precision, double precision, integer);
+-- DROP FUNCTION public.qdgc_getqdgc(double precision, double precision, integer);
 
-CREATE OR REPLACE FUNCTION public.qdgc_get_qdgc(
+CREATE OR REPLACE FUNCTION public.qdgc_getqdgc(
 	lon_value double precision,
 	lat_value double precision,
 	depthlevel integer)
